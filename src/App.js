a7a90f17a59d84d090b811e7ae2d45a8e671771f
MohiuddinRabby <rabbymohiuddin@gmail.com>
2020-03-14 14:47:43 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+// import Contact from './Contact';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const products = [
+        {
+            name: 'Product One',
+            price: 90,
+            date: '10/10/10'
+        }, {
+            name: 'Product Two',
+            price: 100,
+            date: '10/10/10'
+        }, {
+            name: 'Product Three',
+            price: 100,
+            date: '10/10/10'
+        }
+    ];
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Counter></Counter>
+                {products.map(productDetails => < Products p = {
+                    productDetails
+                } > </Products>)}
+            </header>
+        </div>
+    );
+}
+function Counter() {
+    const [count,
+        setCount] = useState(0);
+    // const handleIncrease = () => {setCount(count + 1)}
+    return (
+        <div>
+            <h2>Count: {count}</h2>
+            <button onClick={() => setCount(count + 1)}>Increase</button>
+            <button onClick={() => setCount(count - 1)}>Decrease</button>
+        </div>
+    );
+}
+function Products(props) {
+    const productStyle = {
+        border: '1px solid red',
+        height: '250px',
+        width: '250px',
+        borderRadius: '5px',
+        float: 'left',
+        padding: '5px',
+        margin: '5px'
+    }
+    const {name, price, date} = props.p;
+    // console.log(props)
+    return (
+        <div style={productStyle}>
+            <h3>{name}</h3>
+            <h3>{price}</h3>
+            <h3>{date}</h3>
+            <button>Buy Now</button>
+        </div>
+    );
+
 }
 
 export default App;
